3bfc7fd007fc4e11eca3bd7dc59e8e927dae54d4
samuellucas21504 <samuellucasrdg@gmail.com>
2022-06-09 13:27:48 -0300

    Changed Joke index to a more simple read

--- src/components/Jokes/Joke/index.js
+++ src/components/Jokes/Joke/index.js
@@ -2,18 +2,10 @@ import React from "react";
 import "./style.css";
 
 export default function Joke(props) {
-  if (props.setup) {
     return (
       <div className="joke">
-        <h2>{props.setup}</h2>
+        {props.setup && <h2>{props.setup}</h2>}
         <p>{props.punchline}</p>
       </div>
     );
-  } else {
-    return (
-      <div className="joke">
-        <h2>{props.punchline}</h2>
-      </div>
-    );
-  }
 }
