0d6b3aa5aed5780cd4488e9e933657d6962062db
samuellucas21504 <samuellucasrdg@gmail.com>
2022-06-09 13:43:25 -0300

    Added upvote and downvote system

--- src/components/Jokes/Joke/index.js
+++ src/components/Jokes/Joke/index.js
@@ -4,8 +4,17 @@ import "./style.css";
 export default function Joke(props) {
     return (
       <div className="joke">
-        {props.setup && <h2>{props.setup}</h2>}
-        <p>{props.punchline}</p>
+        {props.setup ? 
+            <>
+                <h2>{props.setup}</h2> 
+                <p>{props.punchline}</p>
+            </>
+        : 
+            <>
+                <h2>{props.punchline}</h2>
+            </>
+        }
+        <p><span className="upvote">{props.upvote}</span> | <span className="downvote">{props.downvote}</span></p>
       </div>
     );
 }
